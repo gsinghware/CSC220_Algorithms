9b1f5ac70c16874182ecdee7e7e51c405eba48e6
Daniel Intskirveli <dintskirveli@appnexus.com>
2014-05-30 07:49:13 -0400

    new backtracking and performance plotting

--- project_3.py
+++ project_3.py
@@ -1,178 +1,256 @@
 import time
 import random
 import sys
+import matplotlib.pyplot as plt
 
+functionToStats = {}
+
+"""
+pad a string with spaces for prettier printing
+"""
 def spacepad(s):
     s= s+":"
     total = 30
     l = len(s)
     return s + "".join([' ' for i in range(total - l)])
 
-def timeit(method):
+"""
+decorator for timing and collecting stats, as well as printing those stats
+as functions run
+"""
+def runtimeandstats(method):
     def timed(*args):
         ts = time.time()
         result = method(*args)
         te = time.time()
-        print '%r (%r) %4.10f sec\t' % \
-              (spacepad(method.__name__), "size="+str(len(args[1])), te-ts)
+        delta = float(te-ts)
+        ms = delta*1000
+        padded_name = spacepad(method.__name__)
+        size = len(args[1])
+        cap_mult = args[0]/size
+        print "{0} problem size={1} capacity={2}*n time={3}".format(padded_name, size, cap_mult, delta)
+        key = "{0}, capacity={1}*n".format(method.__name__,cap_mult)
+        try:
+            functionToStats[key].append( (size,ms) )
+        except KeyError:
+            functionToStats[key] = [ (size,ms) ]
+
         return result
     return timed
 
-# Fractional Knapsack problem
-def _frac_knapsack(M, profit_weight):
+"""
+Wrapper for the greedy algorithm. Call this to run the algorithm on a certain 
+problem with default parameters. 
+"""
+def knapsack_fractional_wrapper(M, profit_weight):
+    l = [(0,0)]+sorted(profit_weight, key=lambda t: float(t[1])/t[0])
+    return knapsack_fractional(len(l), l, M, 0, 0, 0)
+
+"""
+Use a greedy algorithm to solve the fractional knapsack problem. item, weight, and profit
+are parameters so that this function can be used within the backtracking algorithm.
+"""
+def knapsack_fractional(n, profit_weight, capacity, item, weight, profit):
+    potential = profit;
+    totalweight = weight;
+    i = item + 1;
+
+    while i < n and totalweight + profit_weight[i][1] <= capacity :
+        potential += profit_weight[i][0];
+        totalweight += profit_weight[i][1];
+        i+=1
+    if (i < n):
+        potential += (capacity - totalweight) * (float(profit_weight[i][0])/profit_weight[i][1]);
+    return potential
+
+
+"""
+Find the maximum profit for the knapsack problem using dynamic programming.
+"""
+@runtimeandstats
+def knapsack_dynamic(M, profit_weight):
     n = len(profit_weight)
-    l = sorted(profit_weight, key=lambda t: float(t[0])/t[1], reverse=True)
+    #initialize a 0-to-n by 0-to-M, W, matrix with zeros
+    W = [[0] * (M + 1) for i in xrange(n + 1)]
 
-    profit = 0
-
-    for i in range(n):
-        if (l[i][1] > M):
-            break
-        profit += l[i][0]
-        M -= l[i][1]
-
-    if i < n:
-        profit += l[i][0]*(float(M)/l[i][1])
-
-    print "Max Profit: " + str(profit)
-
-
-# 0/1 Knapsack problem using dynamic programming
-@timeit
-def _01_Knapsack(M, profit_weight):
-    n = len(profit_weight)
-    #initialize an n by M matrix with zeros
-    Matrix = [[0] * (M + 1) for i in xrange(n + 1)]
     for i, (profit, weight) in enumerate(profit_weight):
         for capacity in xrange(M + 1):
             if (weight <= capacity):
-                if (profit + Matrix[i][capacity-weight] > Matrix[i][capacity]):
-                    Matrix[i+1][capacity] = profit + Matrix[i][capacity-weight]
-                else:
-                    Matrix[i+1][capacity] = Matrix[i][capacity]
+                W[i+1][capacity] = max(profit + W[i][capacity-weight], W[i][capacity])
             else:
-                Matrix[i+1][capacity] = Matrix[i][capacity]
+                W[i+1][capacity] = W[i][capacity]
+    #return the lower right corner value as the max
+    return W[n][M]
 
-    print "Max Profit: ", str(Matrix[n][M])
 
+"""
+For a size and a capacity multipliers, generate a capacity and a profit_weight array,
+then run the dynamic programming and backtracking algorithms.
 
-# 0/1 Knapsack problem solution using backtracking
+profit_weight is generated randomly, where profit is a random integer between
+10 and 100. Weight is a random integer between 2 and 10.
 
-max_profit = 0
-current_profit = 0.0
-current_weight = 0.0
-
-def pack(i, M, n, profit_weight): 
-    left_c = M - current_weight                             # remaining knapsack capacity 
-    b = current_profit
-    while (i < n and profit_weight[i][1] <= left_c):        # weight of the items decay loaded items 
-        left_c -= profit_weight[i][1]
-        b += profit_weight[i][0] 
-        i += 1
-    
-    if (i < n):
-        b += profit_weight[i][0] * (left_c/profit_weight[i][1])
-    return b
-
-def backtrackto(i, M, n, profit_weight):
-    #print "backtrackto("+", ".join([str(x) for x in [i, M, n, profit_weight]])+")"
-
-    global max_profit
-    global current_profit, current_weight
-
-    if(i > n):
-        if (current_profit > max_profit): 
-            max_profit = current_profit
-        return
-
-    elif (i < n):
-        if (current_weight + profit_weight[i][1] <= M): 
-            current_weight += profit_weight[i][1] 
-            current_profit += profit_weight[i][0] 
-            backtrackto (i + 1, M, n, profit_weight);
-            current_weight -= profit_weight[i][1] 
-            current_profit -= profit_weight[i][0]
-
-    if ((pack(i+1, M, n, profit_weight)) >= max_profit):
-        backtrackto (i+1, M, n, profit_weight)       
-
-@timeit
-def _01_Knapsack_backtracking(M, profit_weight):
-    n = len(profit_weight)
-
-    global max_profit
-
-    temp_profit = 0.0
-    temp_weight = 0.0
-
-    for i in xrange(len(profit_weight)):
-        temp_profit += profit_weight[i][0]
-        temp_weight += profit_weight[i][1]
-
-    global max_profit
-    if (temp_weight <= M):
-        max_profit = temp_profit
-    else:
-        #print "No backtrackto"
-        backtrackto(0, M, n, profit_weight)
-
-    print "Max Profit: " + str(max_profit)
-    max_profit = 0 # for next function call need to set this to zero
-
-def randomTestForN(size):
-    profit_weight1 = []
-    profit_weight2 = []
-    capacity = size*4
+To ensure correctness, there is an assertion that halts the program if the results
+obtained by the two algoorithms differ.
+"""
+def randomTestForN(size, cap_mult):
+    profit_weight = []
+    capacity = size*cap_mult
     for i in range(size):
         profit = random.randint(10,100)
         weight = random.randint(2,10)
-        profit_weight1.append((profit, weight))
-        profit_weight2.append((profit, weight))
+        profit_weight.append((profit, weight))
 
-    _01_Knapsack(capacity, profit_weight1)
-    _01_Knapsack_backtracking(capacity, profit_weight2)
+    max_dynamic = knapsack_dynamic(capacity, list(profit_weight))
+    max_backtracking = knapsack_backtracking_wrapper(capacity, list(profit_weight))
+    assert max_dynamic == max_backtracking
 
-    
-def main():
-    """
-    # Fractional Knapsack problem solution
-    print "Test 1 - Fractional Knapsack problem"
-    M = 20      # capacity
-    n = 3       # size
-    profit_weight = [(25, 18), (24, 15), (15, 10)]
-    _frac_knapsack(M, profit_weight)
-    
-    # 0/1 Knapsack problem using Dynamic Programming
-    print "\nTest 2 - 01 Knapsack problem using dynamic programming"
-    profit_weight_2 = [(3,2), (4,3), (5,4), (6,5)]
-    M_2 = 5
-    n_2 = 4
-    _01_Knapsack(M_2, profit_weight_2)
-
-    print "\nTest 3 - 01 Knapsack problem using dynamic programming"
-    M_3 = 10
-    n_3 = 4
-    profit_weight_3 = [(10,5), (40,4), (30,6), (50,3)]
-    profit_weight_a = [(1,1), (2,2), (3,3), (4,4), (5,5), (6,6), (7,7), (8,8), (9,9), (10,10)]
-    _01_Knapsack(M_3, profit_weight_3)
-    _01_Knapsack(50, profit_weight_a)
-
-    print "\nTest 4 - 01 knapsack problem using backtracking technique"
-    M_4 = 10
-    n_4 = 4
-    profit_weight_4 = [(10,5), (40,4), (30,6), (50,3)]
-    _01_Knapsack_backtracking(M_4, profit_weight_4)
-    #_01_Knapsack_backtracking(50, 10, profit_weight_a)
-    """
-    problem_sizes = [10, 100, 1000]
+"""
+Use system time to measure the performance of backtracking and dynamic programming
+algorithms for some random inputs. For a specific problem size, the problem is created and
+tested by randomTestForN. This just calls that function for several problem sizes and
+capacity multipliers
+Because the dynamic programming algorithms is impacted by changes of the capacity,
+also test for different. See randomTestForN()
+"""
+def performanceTest():
+    problem_sizes = [10, 50, 100, 200, 500, 1000, 2000]
+    cap_mults = [1,2,4]
+    #problem_sizes = [10, 100, 500]
+    #cap_mults = [1, 2, 4]
     for size in problem_sizes:
-        sys.setrecursionlimit(2*size)
-        randomTestForN(size)
-    
+        for cap_mult in cap_mults:
+            sys.setrecursionlimit(3*size)
+            randomTestForN(size, cap_mult)
+    plot()
 
-    _01_Knapsack(15, [(10,2),(5,3),(15,5),(7,7),(6,1),(18,4),(3,1)])
-    _01_Knapsack_backtracking(15, [(10,2),(5,3),(15,5),(7,7),(6,1),(18,4),(3,1)])
 
+"""
+The assignment asked to solve the knapsack problem using the greedy algorithm given
+certain parameters. Do that, and additionally solve the same problem using
+dynamic and backtracking algorithms.
+"""
+def assignment():
+    capacity = 15
+
+    profits = [10, 5, 15, 7, 6, 18, 3]
+    weights = [2, 3, 5, 7, 1, 4, 1]
+
+    profit_weight = zip(profits, weights)
+
+    val1 = knapsack_backtracking_wrapper(capacity, profit_weight);
+    val2 = knapsack_dynamic(capacity, profit_weight)
+
+    val3 = knapsack_fractional_wrapper(capacity, profit_weight)
+    print "solution using fractional knapsack algorithm: "+str(val3)
+
+    #make sure we got the same answer for dynamic and backtracking solutions
+    assert val1 == val2
+    print "solution using dynamic and backtracking algorithms: "+str(val2)
+
+    #clear stats so as to not pollute the plot
+    functionToStats = {}
+
+
+maxProfit = 0.0
+kept = []
+
+"""
+Wrapper for starting the knapsack backtracking.
+Does setup by sorting profit_weight, finding its length, prepending a dummy value to it,
+resetting global variables, and starting the search with appropriate starting parameters
+for knapsack_backtracking()
+"""
+@runtimeandstats
+def knapsack_backtracking_wrapper(M, profit_weight):
+    #prepend a dummy value to our list of pofit,weight sorted by profit/weight non-decreased
+    items = [(0,0)] + sorted(profit_weight, key=lambda t: float(t[0])/t[1], reverse=True)
+
+    #reset global variables
+    global maxProfit
+    global kept
+    maxProfit = 0.0
+    kept = []
+
+    #kick off the dfs for the implicit backtracking state tree
+    knapsack_backtracking(len(items), items, M, 0, 0, 0, [])
+    return maxProfit
+
+"""
+knapsack problem solution:
+1) Check if the current solution is the best one. If so, keep it
+2) Check if the current node is promsing
+    -if it is, continue the search. First on the left child, then on the right
+    -left child represents including the item at index
+    -right child represents leaving it out of the solution
+    -if this node is not promising, do nothing (prune the state tree here)
+"""
+def knapsack_backtracking(n, profit_weight, capacity, index, weight, profit, curList):
+    global maxProfit
+    global kept
+
+    if weight <= capacity and  profit > maxProfit :
+        #this is currently the best we can do! save it... 
+        maxProfit = profit;
+        kept = curList
+    #check if this node is promising
+    if promising(n, profit_weight, index, capacity, weight, profit):
+        leftList = list(curList)
+        leftList.append(index + 1);
+        #depth first search the left node (where we keep the next element)
+        knapsack_backtracking(n, profit_weight, capacity, index + 1, weight + profit_weight[index+1][1], profit + profit_weight[index+1][0], leftList);
+        rightList = list(curList)
+        #depth first search the right node (where we dont keep the next element)
+        knapsack_backtracking(n, profit_weight, capacity, index + 1, weight, profit, rightList);
+
+"""
+Is this node promising? i.e. Is the current weight less than the sack's capacity?
+Does the maximum potential value of the children nodes (the "potential") exceed 
+the maximum profit we've observed so far? This bound is calculated in the same way 
+that the fractional knapsack problem is solved. The actual maximum value under this 
+node is propably not as good as the bound we calculate here, but this works as a way
+to prune the state tree.
+"""
+def promising(n, profit_weight, item, capacity, weight, profit):
+    #our current weight already exceeds the capacity, prune this node
+    if (weight > capacity) :
+        return False;
+    #get the potential max. value using the fractional knapsack solution
+    potential = knapsack_fractional(n, profit_weight, capacity, item, weight, profit)
+    return potential > maxProfit;
+
+"""
+create a plot from the stats collected by the @runtimeandstats decorator
+"""
+def plot():
+    fig = plt.figure()
+    dpi = fig.dpi 
+    fig.set_size_inches(800 / dpi, 600 / dpi) 
+    plt.ylabel("run time (ms)")
+    plt.xlabel("problem size")
+
+    for key in functionToStats:
+        points = functionToStats[key]
+        x = [t[0] for t in points]
+        y = [t[1] for t in points]
+        if key.find('dyn') != -1:
+            style = '--'
+            w = 2
+        else:
+            style = ''
+            w =1
+        line = plt.plot(x, y, style, label=key, linewidth=w)
+
+    plt.title("knapsack problem: dynamic prog. vs. backtracking performance")
+    plt.legend(loc="best", prop={'size':10})
+    plt.savefig("stats", bbox_inches='tight')
+
+"""
+Solve the assigned problem, and test performance for dynamic and backtracking algorithms
+"""
+def main():
+    assignment()
+    performanceTest()
 
 if __name__ == '__main__':
     main()
